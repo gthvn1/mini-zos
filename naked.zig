c309499865981679d7810eb4b8c1e0a5e5542ee7
Guillaume <guillaume.thouvenin@vates.tech>
2023-10-26 18:19:57 +0200

    add space for stack

--- naked.zig
+++ naked.zig
@@ -1,3 +1,11 @@
+export var stack: [8 * 1024]u8 align(16) linksection(".bss") = undefined;
+var stack_top: usize = undefined;
+
 export fn _start() callconv(.Naked) noreturn {
+    stack_top = @intFromPtr(&stack) + stack.len;
+    asm volatile (""
+        :
+        : [stack_top] "{rsp}" (stack_top),
+    );
     while (true) {}
 }
