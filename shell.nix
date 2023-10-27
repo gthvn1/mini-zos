60dd84ab4d64024714c55de71051392d5f2d95ac
Guillaume <guillaume.thouvenin@vates.tech>
2023-10-27 11:51:07 +0200

    install helix zig and zls

---
+++ shell.nix
@@ -0,0 +1,6 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [ helix zig zls ];
+}
+
