d2f20a8863927354b29d2822d3bed446c5ed19f8
Guillaume <guillaume.thouvenin@vates.tech>
2023-10-28 20:54:24 +0200

    Setup stack
    It seems that @call cannot be use anymore. So use asm volatile to setup
    the stack and once done call kernel main code.

--- src/mini-zos.zig
+++ src/mini-zos.zig
@@ -1,5 +1,6 @@
 const startInfo = @import("start_info.zig");
 
+// the stack should be aligned to 16-bytes boundary.
 export var stack: [8 * 1024]u8 align(16) linksection(".bss") = undefined;
 var stack_top: usize = undefined;
 
@@ -9,11 +10,24 @@ var stack_top: usize = undefined;
 // Look in xen source: xen/arch/x86/guest/xen/hypercall_page.S
 export var hypercall_page: [4 * 1024]u8 linksection(".hypercall_page") = undefined;
 
+// See modifiers for inline asm
+// https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#x86Operandmodifiers
 export fn _start() callconv(.Naked) noreturn {
     stack_top = @intFromPtr(&stack) + stack.len;
-    asm volatile (""
-        :
-        : [stack_top] "{rsp}" (stack_top),
+    asm volatile (
+        \\mov %[stack_top], %%rsp
+        \\call %[kernelStart:P]
+        : // output operands
+        : [stack_top] "r" (stack_top), //input operands
+          [kernelStart] "X" (&kMain),
+        : "rsp " //Clobbers
     );
-    while (true) {}
+
+    while (true)
+        asm volatile ("hlt");
+}
+
+fn kMain() void {
+    // add nop nop to help to spot it if disassemble...
+    asm volatile ("nop; nop");
 }
