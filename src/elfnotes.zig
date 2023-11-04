f7f421144c98a35ae6c74952353d8d624dacf697
9u1llØm3 <gthvn1@gmail.com>
2023-11-04 11:01:43 +0100

    1 add elfnote (#8)
    This patch adds ELF note for Xen. Asciz notes are generated from Zig but 
    the note for hypercall address is done in assembly because we don't know
    the address of the symbol at comptime.

---
+++ src/elfnotes.zig
@@ -0,0 +1,50 @@
+// The notes should live in a PT_NOTE segment and have "Xen" in the name field
+// ASCIZ type are string NULL terminated.
+
+// See mini-os include/xen/elfnote.h
+const XenElfNote = enum {
+    xen_version,
+    guest_os,
+    loader,
+
+    pub fn toString(note: XenElfNote) []const u8 {
+        return switch (note) {
+            .xen_version => "5",
+            .guest_os => "6",
+            .loader => "8",
+        };
+    }
+
+    pub inline fn pushsection() []const u8 {
+        return 
+        \\    .pushsection .note.Xen
+        \\    .align 4
+        \\    .long 2f - 1f
+        \\    .long 4f - 3f
+        \\
+        ;
+    }
+
+    pub inline fn middlesection() []const u8 {
+        return 
+        \\1:.asciz #Xen
+        \\2:.align 4
+        \\
+        ;
+    }
+    pub inline fn popsection() []const u8 {
+        return 
+        \\4:.align 4
+        \\    .popsection
+        \\
+        ;
+    }
+};
+
+pub inline fn genXen(comptime value: XenElfNote, comptime desc: []const u8) void {
+    asm volatile (XenElfNote.pushsection() ++
+            "    .long " ++ value.toString() ++ "\n" ++ // type
+            XenElfNote.middlesection() ++
+            "3:.asciz \"" ++ desc ++ "\"\n" ++
+            XenElfNote.popsection());
+}
--- src/kmain.zig
+++ src/kmain.zig
@@ -1,23 +1,48 @@
 const startInfo = @import("startinfo.zig");
+const elfNote = @import("elfnotes.zig");
 
 // the stack should be aligned to 16-bytes boundary.
 export var stack: [8 * 1024]u8 align(16) linksection(".bss") = undefined;
 
-// Hypercall is done by mapping an hypercall page in mini-zos address
-// space. The hypercall_page will be filled by Xen when it will load
-// us. We just need to keep space at 0x3000
-// Look in xen source: xen/arch/x86/guest/xen/hypercall_page.S
-export var hypercall_page: [4 * 1024]u8 linksection(".hypercall_page") = undefined;
+extern const hypercall_page: usize;
+extern const shared_info: usize;
 
-// See modifiers for inline asm
-// https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#x86Operandmodifiers
 export fn _start() callconv(.Naked) noreturn {
     const stack_top: usize = @intFromPtr(&stack) + stack.len;
+
+    // See modifiers for inline asm
+    // https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#x86Operandmodifiers
     asm volatile (
         \\call %[kmain:P]
         : // output operands
         : [_] "{rsp}" (stack_top), //input operands [setup the stack]
           [kmain] "X" (&kMain),
+    );
+
+    elfNote.genXen(.xen_version, "xen-3.0");
+    elfNote.genXen(.guest_os, "Mini-ZOS");
+    elfNote.genXen(.loader, "generic");
+    // Need to make room for the shared info page and hypercall page.
+    // Look in xen source: xen/arch/x86/guest/xen/hypercall_page.S
+    asm volatile (
+        \\
+        \\    .pushsection .note.Xen
+        \\    .align 4
+        \\    .long 2f - 1f
+        \\    .long 4f - 3f
+        \\    .long 2
+        \\1:.asciz #Xen
+        \\2:.align 4
+        \\3:.quad hypercall_page
+        \\4:.align 4
+        \\    .popsection
+        \\
+        \\.globl shared_info, hypercall_page
+        \\    .align 4096
+        \\shared_info:
+        \\    .fill 4096,1,0
+        \\hypercall_page:
+        \\    .fill 4096,1,0
     );
 
     while (true)
